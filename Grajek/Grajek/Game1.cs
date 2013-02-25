10342801b0e18163ed0b41424d8fc3913b970270
Jan Korzeniowski <j.korzeniowski@lgbs.pl>
2013-02-26 00:24:27 +0100

    Proba zrobienia edycji

--- Grajek/Grajek/Game1.cs
+++ Grajek/Grajek/Game1.cs
@@ -34,9 +34,17 @@ namespace Grajek
         private bool[] sprawdzKlawisz;
         private List<Vector2> listaKlawiszy = new List<Vector2>();
         private Synth synchronizator;
+        
         private Rectangle nagrywanieButton = new Rectangle(10, 390, 180, 80);
-        private Rectangle nagrywanieStopButton = new Rectangle(210, 390, 180, 80);
-        private Rectangle odtwarzanieButton = new Rectangle(420, 390, 360, 80);
+        private Rectangle nagrywanieStopButton = new Rectangle(196, 390, 180, 80);
+        private Rectangle odtwarzanieButton = new Rectangle(382, 390, 180, 80);
+        private Rectangle edycjaButton = new Rectangle(610, 390, 180, 80);
+
+        private Rectangle aButton = new Rectangle(10, 390, 144, 80);
+        private Rectangle bButton = new Rectangle(160, 390, 144, 80);
+        private Rectangle delButton = new Rectangle(310, 390, 144, 80);
+        private Rectangle acButton = new Rectangle(460, 390, 144, 80);
+        private Rectangle backButton = new Rectangle(640, 390, 144, 80);
 
         private List<Klawisz> ListaNagrania; // Lista zapamiêtuje kolejno naciskane klawisze. Pos³u¿y do nagrywania
         int j = 0;
@@ -44,9 +52,15 @@ namespace Grajek
         private bool playing = false;
         private bool played = false;
         private bool recorded = false;
+        private bool edit = false;
+        private bool endedit = true;
         private float timer; // Timer do odliczenia jak d³ugo by³ naciœniêty przycisk
 
         private bool press = false; //pomocniczy
+        private float timerStart =0;
+        private float timerEnd = 0;
+        
+        
         private enum OscillatorTypes
         {
             Sine,
@@ -122,21 +136,21 @@ namespace Grajek
                     synchronizator.NoteOn(aktualnyNrNuty);
                     nacisnietoKlawisz(aktualnyNrNuty);
                 }
-
+                
                 if (record == true)
                 {
                     timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds; //Rozpoczêcie naliczania "czasu przyciœciêcia klawisza"
                     press = true; // do nagrywania
                 }
-
+                
                 played = true;
             }
             else if (ms.LeftButton == ButtonState.Released)
             {
+                
                 synchronizator.NoteOff(aktualnyNrNuty);
                 resetujKlawisz(aktualnyNrNuty);
-
-
+                
                 if (nagrywanieButton.Contains(new Point(ms.X, ms.Y)) && record == false)
                 {
                     recorded = true;
@@ -162,6 +176,56 @@ namespace Grajek
                     Play();
                     //recorded = true;
                     // playing = false;
+                }
+
+                if ( edycjaButton.Contains(new Point(ms.X, ms.Y)))
+                {
+                    edit = true;
+                    endedit = false;
+                }
+
+                if (edit == true && endedit== false)
+                {
+                    int wskaznik = 0;
+                    if (aButton.Contains(new Point(ms.X, ms.Y)))
+                    {
+                       // 1. Naciskam A
+                       // 2. Biore z listyNagran nr klawisza
+                       // 3. Odgrywam go poki nie puszcze A
+                       // 4. Wpisuje w 1 miejsce listy, taki sam klawisz ze zmienionym czasem trwania
+                        
+                    }
+
+                    if (bButton.Contains(new Point(ms.X, ms.Y)))
+                    {
+                        // To samo co w A, ale kolejny z kolei czyli 2 klawisz
+                    }
+
+                    if (delButton.Contains(new Point(ms.X, ms.Y)))
+                    {
+                        int a = ListaNagrania.Count;
+                        int poz = -1;
+                        if (a > 0)
+                        {
+                            for (int i = 0; i < a; i++)
+                            {
+                                if (ListaNagrania[i].nr_klawisza != 0)
+                                    poz = i;
+                            }
+                            if (poz > -1) 
+                                ListaNagrania.RemoveAt(poz);       
+                        }
+                    }
+
+                    if (acButton.Contains(new Point(ms.X, ms.Y)))
+                    {
+                        ListaNagrania.Clear();
+                    }
+
+                    if (backButton.Contains(new Point(ms.X, ms.Y)))
+                    {
+                        //endedit = true; // tak jakby endedit == (edit = false)
+                    }
                 }
 
                 if (record == true && press == true)
@@ -263,7 +327,9 @@ namespace Grajek
                 }
             }
             else if (sprawdzKlawisz.Length > nrNuty)
+            {
                 sprawdzKlawisz[nrNuty] = true;
+            }
         }
 
         protected void resetujKlawisz(int nrNuty)
@@ -364,20 +430,40 @@ namespace Grajek
                         sprawdzKlawisz[i] ? Color.Aqua : Color.Black, 0.0f, Vector2.Zero, SpriteEffects.None, 0.7f);
                 }
             }
-
-            if (record == true) spriteBatch.Draw(tekstura, nagrywanieButton, null, Color.Gray, 0, Vector2.Zero, SpriteEffects.None, 1);
-            if (record == false) spriteBatch.Draw(tekstura, nagrywanieButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
-            spriteBatch.DrawString(Font1, "Record", new Vector2(55, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-
-            spriteBatch.Draw(tekstura, nagrywanieStopButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
-            spriteBatch.DrawString(Font1, "Stop", new Vector2(270, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-
-            if (playing == true) spriteBatch.Draw(tekstura, odtwarzanieButton, null, Color.Gray, 0, Vector2.Zero, SpriteEffects.None, 1);
-            if (playing == false)
+            if (edit == false || (endedit == true && edit == true))
             {
-                spriteBatch.Draw(tekstura, odtwarzanieButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                if (record == true) spriteBatch.Draw(tekstura, nagrywanieButton, null, Color.Gray, 0, Vector2.Zero, SpriteEffects.None, 1);
+                if (record == false) spriteBatch.Draw(tekstura, nagrywanieButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "Record", new Vector2(55, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+                spriteBatch.Draw(tekstura, nagrywanieStopButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "Stop", new Vector2(255, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+                if (playing == true) spriteBatch.Draw(tekstura, odtwarzanieButton, null, Color.Gray, 0, Vector2.Zero, SpriteEffects.None, 1);
+                if (playing == false) spriteBatch.Draw(tekstura, odtwarzanieButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "Play", new Vector2(445, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+                spriteBatch.Draw(tekstura, edycjaButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "EDIT", new Vector2(670, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
             }
-            spriteBatch.DrawString(Font1, "Play", new Vector2(570, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+            if ( edit == true && endedit == false )
+            {
+                spriteBatch.Draw(tekstura, aButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "A", new Vector2(75, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+                spriteBatch.Draw(tekstura, bButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "B", new Vector2(225, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+                spriteBatch.Draw(tekstura, delButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "Del", new Vector2(360, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+                spriteBatch.Draw(tekstura, acButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "AC", new Vector2(515, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+                spriteBatch.Draw(tekstura, backButton, null, Color.Snow, 0, Vector2.Zero, SpriteEffects.None, 1);
+                spriteBatch.DrawString(Font1, "BACK", new Vector2(680, 410), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+            }
+
             spriteBatch.End();
 
             base.Draw(gameTime);
